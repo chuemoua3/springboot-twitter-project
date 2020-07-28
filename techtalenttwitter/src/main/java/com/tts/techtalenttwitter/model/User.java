14d9231f70c1b56f33a575b6fd4c91292353b89c
chuemoua3 <chuemoua12@outlook.com>
2020-07-28 11:58:24 -0400

    3rd commit

--- techtalenttwitter/src/main/java/com/tts/techtalenttwitter/model/User.java
+++ techtalenttwitter/src/main/java/com/tts/techtalenttwitter/model/User.java
@@ -46,7 +46,6 @@ public class User {
     
     @NotEmpty(message = "Please provide a username")
     @Length(min = 3, message = "Your username must have at least 3 characters")
-    @Length(max = 15, message = "Your username cannot have more than 15 characters")
     @Pattern(regexp="[^\\s]+", message="Your username cannot contain spaces")
     private String username;
     

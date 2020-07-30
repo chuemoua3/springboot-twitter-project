fc40c9c90d474b62cf591833bcc67a038fc1b4c0
chuemoua3 <chuemoua12@outlook.com>
2020-07-30 10:39:34 -0400

    4th commit

--- techtalenttwitter/src/main/java/com/tts/techtalenttwitter/controller/TweetController.java
+++ techtalenttwitter/src/main/java/com/tts/techtalenttwitter/controller/TweetController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tts.techtalenttwitter.model.Tweet;
@@ -20,23 +21,23 @@ import com.tts.techtalenttwitter.service.UserService;
 public class TweetController {
     @Autowired
     private UserService userService;
-	
+
     @Autowired
     private TweetService tweetService;
-    
-    @GetMapping(value= {"/tweets", "/"})
-    public String getFeed(Model model){
+
+    @GetMapping(value = { "/tweets", "/" })
+    public String getFeed(Model model) {
         List<Tweet> tweets = tweetService.findAll();
         model.addAttribute("tweetList", tweets);
         return "feed";
     }
-    
+
     @GetMapping(value = "/tweets/new")
     public String getTweetForm(Model model) {
         model.addAttribute("tweet", new Tweet());
         return "newTweet";
     }
-    
+
     @PostMapping(value = "/tweets")
     public String submitTweetForm(@Valid Tweet tweet, BindingResult bindingResult, Model model) {
         User user = userService.getLoggedInUser();
@@ -47,5 +48,13 @@ public class TweetController {
             model.addAttribute("tweet", new Tweet());
         }
         return "newTweet";
+    }
+
+    @GetMapping(value = "/tweets/{tag}")
+    public String getTweetsByTag(@PathVariable(value = "tag") String tag, Model model) {
+        List<Tweet> tweets = tweetService.findAllWithTag(tag);
+        model.addAttribute("tweetList", tweets);
+        model.addAttribute("tag", tag);
+        return "taggedTweets";
     }
 }
